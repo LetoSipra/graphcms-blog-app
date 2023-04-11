9bad3966a16ec5487705d41c448cb94689f1a83d
LetoSipra(Yusuf) <yusufakcay22.0@gmail.com>
2023-04-12 01:52:33 +0300

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,11 @@ import React from "react";
 function Header() {
   return (
     <>
-      <header className="flex h-16 w-full items-center bg-white/10 border-b">
+      <header className="flex h-16 w-full items-center border-b border-b-white/20 bg-white/10">
         <Link href={"/"}>
-          <h1 className="mx-5 md:mx-10 my-auto text-2xl text-[#FF00FF]">CodeBlog</h1>
+          <h1 className="mx-5 my-auto text-2xl text-[#FF00FF] md:mx-10">
+            CodeBlog
+          </h1>
         </Link>
       </header>
     </>
